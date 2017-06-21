9d7cb0e01b782e7b47d9613c904cb87fb00c9c65
mogwai123 <alexfdzprieto@gmail.com>
2017-06-21 22:49:21 +0200

    Cambiar regex

--- src/utils/validatorUtils.ts
+++ src/utils/validatorUtils.ts
@@ -1,4 +1,4 @@
 export class ValidatorUtils {
-  public static REGEX_ALPHANUMERIC =  '[a-zA-Z ]*';
+  public static REGEX_ALPHANUMERIC =  '[a-zA-Z0-9]*$';
   public static MIN_SIZE_PASSWORD  = 4;
 }
