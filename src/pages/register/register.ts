9ca873c8b21b81ca5f78b3f263d853a5781c342b
mogwai123 <alexfdzprieto@gmail.com>
2017-07-04 21:39:26 +0200

    Inicio registro

--- src/pages/register/register.ts
+++ src/pages/register/register.ts
@@ -1,24 +1,63 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { NavController, AlertController, IonicPage } from 'ionic-angular';
+import { AuthServiceProvider } from '../../providers/auth-service/auth-service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ValidatorUtils } from '../../utils/validatorUtils'
+import { User } from '../../model/entity/User';
 
-/**
- * Generated class for the RegisterPage page.
- *
- * See http://ionicframework.com/docs/components/#navigation for more info
- * on Ionic pages and navigation.
- */
 @IonicPage()
 @Component({
   selector: 'page-register',
-  templateUrl: 'register.html',
+  templateUrl: 'register.html'
 })
 export class RegisterPage {
+  createSuccess = false;
+  registerCredentials : User;
+  registerPageForm: FormGroup;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(private nav: NavController, private auth: AuthServiceProvider,
+    private alertCtrl: AlertController , public formBuilder: FormBuilder) {
+      this.init();
+    }
+
+
+    public register() {
+      this.auth.register(this.registerCredentials).subscribe(success => {
+        if (success) {
+          this.createSuccess = true;
+          this.showPopup("Success", "Account created.");
+        } else {
+          this.showPopup("Error", "Problem creating account.");
+        }
+      },
+      error => {
+        this.showPopup("Error", error);
+      });
+    }
+
+    public init() {
+      this.registerCredentials = new User('', '');
+      this.registerPageForm = this.formBuilder.group({
+        username: ['', Validators.compose([Validators.pattern(ValidatorUtils.REGEX_ALPHANUMERIC), Validators.required])],
+        password: ['', Validators.compose([Validators.minLength(ValidatorUtils.MIN_SIZE_PASSWORD), Validators.required])]
+      });
+    }
+
+    showPopup(title, text) {
+      let alert = this.alertCtrl.create({
+        title: title,
+        subTitle: text,
+        buttons: [
+          {
+            text: 'OK',
+            handler: data => {
+              if (this.createSuccess) {
+                this.nav.popToRoot();
+              }
+            }
+          }
+        ]
+      });
+      alert.present();
+    }
   }
-
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad RegisterPage');
-  }
-
-}
